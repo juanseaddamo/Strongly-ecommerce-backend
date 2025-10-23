1ea6629948322cd0107244be015070bd2e2103bb
juan segundo addamo <addamojuanse@gmail.com>
2025-10-23 07:23:23 -0300

    mods

--- Strongly-backend/demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
+++ Strongly-backend/demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
@@ -28,37 +28,41 @@ public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http
-                                .csrf(AbstractHttpConfigurer::disable)
-                                .authorizeHttpRequests(req -> req
-                                 // Endpoints públicos                                 
-                                 .requestMatchers("/api/v1/auth/**").permitAll() // auth
-                                .requestMatchers("/error/**").permitAll() // manejo de errores
-                                .requestMatchers(HttpMethod.GET,"/product", "/product/**").permitAll() // listar productos 
-                                .requestMatchers(HttpMethod.GET,"/categories/**").permitAll() // categorías 
-                                .requestMatchers(HttpMethod.GET,"/offers/**").permitAll() // ofertas 
-                                .requestMatchers(HttpMethod.GET,"/brands/**").permitAll() // marcas 
-                                .requestMatchers(HttpMethod.GET,"/support/**").permitAll() // soporte 
-                                // Endpoints solo para compradores                                
-                                .requestMatchers("/cart/**").hasAnyAuthority(Role.BUYER.name()) // carrito
-                                .requestMatchers("/orders/**").hasAuthority(Role.BUYER.name()) // órdenes
-                                .requestMatchers("/users/profile/**").hasAuthority(Role.BUYER.name()) // perfil
-                                 // Endpoints solo para vendedores/admin
-                                .requestMatchers(HttpMethod.POST,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear productos
-                                .requestMatchers(HttpMethod.PUT,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // actualizar productos                                
-                                .requestMatchers("/inventory/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // inventario
-                                .requestMatchers(HttpMethod.POST,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
-                                .requestMatchers(HttpMethod.PUT,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
-                                // Endpoints solo para admin
-                                .requestMatchers(HttpMethod.DELETE,"/product/**").hasAuthority(Role.ADMIN.name()) // eliminar productos
-                                .requestMatchers("/users/**").hasAnyAuthority(Role.ADMIN.name(),Role.SELLER.name(), Role.BUYER.name()) // gestión de usuarios
-                                .requestMatchers(HttpMethod.DELETE,"/offers/**").hasAuthority(Role.ADMIN.name()) // eliminar ofertas
-                                .anyRequest()
-                                .authenticated())
-                                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                                .authenticationProvider(authenticationProvider)
-                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+            // Endpoints públicos                                 
+            .requestMatchers("/api/v1/auth/**").permitAll() // auth
+            .requestMatchers("/error/**").permitAll() // manejo de errores
+            .requestMatchers(HttpMethod.GET,"/product", "/product/**").permitAll() // listar productos 
+            .requestMatchers(HttpMethod.GET,"/categories/**").permitAll() // categorías 
+            .requestMatchers(HttpMethod.GET,"/offers/**").permitAll() // ofertas 
+            .requestMatchers(HttpMethod.GET,"/brands/**").permitAll() // marcas 
+            .requestMatchers(HttpMethod.GET,"/support/**").permitAll() // soporte 
+            // Endpoints solo para compradores                                
+            .requestMatchers("/cart/**").hasAnyAuthority(Role.BUYER.name()) // carrito
+            .requestMatchers("/orders/**").hasAuthority(Role.BUYER.name()) // órdenes
+            .requestMatchers("/users/profile/**").hasAuthority(Role.BUYER.name()) // perfil
+            // Endpoints solo para vendedores/admin
+            .requestMatchers(HttpMethod.POST,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear productos
+            .requestMatchers(HttpMethod.PUT,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // actualizar productos                                
+            .requestMatchers("/inventory/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // inventario
+            .requestMatchers(HttpMethod.POST,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
+            .requestMatchers(HttpMethod.PUT,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
+            // Endpoints solo para admin
+            .requestMatchers(HttpMethod.DELETE,"/product/**").hasAuthority(Role.ADMIN.name()) // eliminar productos
+            .requestMatchers("/users/**").hasAnyAuthority(Role.ADMIN.name(),Role.SELLER.name(), Role.BUYER.name()) // gestión de usuarios
+            .requestMatchers(HttpMethod.DELETE,"/offers/**").hasAuthority(Role.ADMIN.name()) // eliminar ofertas
+            .anyRequest()
+            .authenticated()
+        )
+        .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-                return http.build();
-        }
+    return http.build();
+}
+
+
+
 }
