4f44f41e2a7eec9965117216e24c6700af64a2b4
juan segundo addamo <addamojuanse@gmail.com>
2025-10-23 00:43:58 -0300

    cambio de configuracion en seguridad de permisos de usuario (security config)

--- Strongly-backend/demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
+++ Strongly-backend/demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                                  // Endpoints públicos                                 
                                  .requestMatchers("/api/v1/auth/**").permitAll() // auth
                                 .requestMatchers("/error/**").permitAll() // manejo de errores
-                                .requestMatchers(HttpMethod.GET,"/products/**").permitAll() // listar productos 
+                                .requestMatchers(HttpMethod.GET,"/product/**").permitAll() // listar productos 
                                 .requestMatchers(HttpMethod.GET,"/categories/**").permitAll() // categorías 
                                 .requestMatchers(HttpMethod.GET,"/offers/**").permitAll() // ofertas 
                                 .requestMatchers(HttpMethod.GET,"/brands/**").permitAll() // marcas 
@@ -44,14 +44,14 @@ public class SecurityConfig {
                                 .requestMatchers("/orders/**").hasAuthority(Role.BUYER.name()) // órdenes
                                 .requestMatchers("/users/profile/**").hasAuthority(Role.BUYER.name()) // perfil
                                  // Endpoints solo para vendedores/admin
-                                .requestMatchers(HttpMethod.POST,"/products/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear productos
-                                .requestMatchers(HttpMethod.PUT,"/products/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // actualizar productos                                
+                                .requestMatchers(HttpMethod.POST,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear productos
+                                .requestMatchers(HttpMethod.PUT,"/product/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // actualizar productos                                
                                 .requestMatchers("/inventory/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // inventario
                                 .requestMatchers(HttpMethod.POST,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
                                 .requestMatchers(HttpMethod.PUT,"/offers/create/**").hasAnyAuthority(Role.SELLER.name(), Role.ADMIN.name()) // crear ofertas
                                 // Endpoints solo para admin
-                                .requestMatchers(HttpMethod.DELETE,"/products/**").hasAuthority(Role.ADMIN.name()) // eliminar productos
-                                .requestMatchers("/users/**").hasAuthority(Role.ADMIN.name()) // gestión de usuarios
+                                .requestMatchers(HttpMethod.DELETE,"/product/**").hasAuthority(Role.ADMIN.name()) // eliminar productos
+                                .requestMatchers("/users/**").hasAnyAuthority(Role.ADMIN.name(),Role.SELLER.name(), Role.BUYER.name()) // gestión de usuarios
                                 .requestMatchers(HttpMethod.DELETE,"/offers/**").hasAuthority(Role.ADMIN.name()) // eliminar ofertas
                                 .anyRequest()
                                 .authenticated())
